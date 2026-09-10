880d2758df83c1603afacce0fa08d65a51a2c00a
diufs <50545332+diufs@users.noreply.github.com>
2026-09-10 13:37:07 +0200

    Add files via upload

---
+++ Bienvenido.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bienvenido;
+import java.util.Scanner;
+/**
+ *
+ * @author ivadro2
+ */
+public class Bienvenido {
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        char car;
+        
+        System.out.println("Por favor introduce una letra");
+        car = sc.next().charAt(0);
+        
+        System.out.printf("\nHas puesto el número \"%c\"", car);
+        
+        
+        
+        /*
+       
+        int n;
+        System.out.println("Por favor introduce un numero entero");
+        n =sc.nextInt();
+        
+        System.out.print("Otro mensaje!!");
+        System.out.println("Hola Mundo"); //line new 
+        System.out.print("Otro mensaje mas !!!");
+        System.out.printf("Aqui otro  \ndistinto a los anteriores"); // Funciones como el print
+        int res = 1005;
+        System.out.println("\nelvalor de res es "+ res);
+        System.out.printf("\nEl valor de res es %d", res);
+        
+        System.out.println("\nElvalor " + res + " correspode a res");
+        System.out.printf("\nElvalor %03d  correspode a res", res);
+    
+        float pi = 3.14f;
+        System.out.printf("\nPi vale:  %.2f", pi);
+*/
+    
+    
+    }
+}
+
+
