d63889065597cb97271cd0f45696ae96f3fab646
diufs <50545332+diufs@users.noreply.github.com>
2026-09-11 13:47:15 +0200

    Add files via upload

---
+++ Ejercicio1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicio1;
+
+/**
+ *
+ * @author ivadro2
+ */
+public class Ejercicio1 {
+
+    public static void main(String[] args) {
+        int num;
+        num = 1000;
+        //Es una secuencia de escape para los tipos decimales %d
+        System.out.printf("El valor es %d", num);
+      
+    }
+}
---
+++ Ejercicio2.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicio2;
+
+/**
+ *
+ * @author ivadro2
+ */
+public class Ejercicio2 {
+
+    public static void main(String[] args) {
+       
+        //Declaramos las variables
+       double d;
+       char c;
+       float f;
+       //Asignamos el valor
+       f=1000.521f;
+       d = 500.123;
+       c = 'Y';
+       
+       //Imprimimos con printf y una secuencia de escape
+       System.out.printf(" f es %f", f);
+       System.out.printf(" d es %f", d);
+       System.out.printf(" c es %c", c);
+    }
+}
